9308acf7cf885a707ccd9027a003681de210348f
algammal <algammal@eg.ibm.com>
2020-04-18 11:44:16 +0200

    fixed 404 issue

--- src/components/PageNotFound.js
+++ src/components/PageNotFound.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const PageNotFound = () => (
     <div className="text-center page-not-found-image-container">
-        <img className="page-not-found-image" src="/404.jpg" alt="" />
+        <img className="page-not-found-image" src="/404.jpg" alt="404" />
     </div>
 );
 
